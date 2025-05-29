0c434ec8bf1808ff2769fd6dbacf18f48c7f7d41
Dhruva-007 <ghantasaladhruvann@gmail.com>
2025-05-29 12:12:52 +0530

    Supabase url added
    --Error while 'npm run build', so added supabase urls

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -13,6 +13,6 @@ if (!supabaseUrl || !supabaseAnonKey) {
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  supabaseUrl || "https://your-project-url.supabase.co",
-  supabaseAnonKey || "your-anon-key",
+  supabaseUrl || "https://qkmuehomuwzzxfkansbw.supabase.co",
+  supabaseAnonKey || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrbXVlaG9tdXd6enhma2Fuc2J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MzY0MDUsImV4cCI6MjA2MzUxMjQwNX0.1slVu00Me8OQs7YxMpjrNKGbtAlvQiTnd-vZPq34XPI",
 )
